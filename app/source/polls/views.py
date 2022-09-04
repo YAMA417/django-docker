37d46a00680d3b07cf5ac9b624c3e821d4f96a68
kenita07 <k.itakura07@gmail.com>
2022-09-04 23:29:17 +0900

    fix tutorial 3

--- app/source/polls/views.py
+++ app/source/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
